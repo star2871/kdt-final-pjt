c5f72154c00622223410ef2fba62c089eb4a8b0a
star2871 <star2871@gmail.com>
2022-11-29 14:48:19 +0900

    Feat: accounts detail 페이지 이미지 띄우기 완료

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth import get_user_model
 
 
@@ -13,6 +13,16 @@ class CustomUserCreationForm(UserCreationForm):
 
         labels = {
             "username": "아이디",
+            "nick_name": "닉네임",
+            "profile_image": "프로필이미지",
+        }
+
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = get_user_model()
+        fields = ("nick_name", "profile_image")
+        labels = {
             "nick_name": "닉네임",
             "profile_image": "프로필이미지",
         }
