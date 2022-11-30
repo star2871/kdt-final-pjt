88cb0d2e292af3f26db39d7931e65a9dc7f2ab9c
runedemonic <runedemonic@naver.com>
2022-11-30 13:37:09 +0900

    Test: Google Calendar 인증 및 이벤트 객체생성 테스트

--- communities/tests.py
+++ communities/tests.py
@@ -1,3 +1,48 @@
 from django.test import TestCase
+from google_auth_oauthlib.flow import InstalledAppFlow
+import datetime
+# 구글 캘린더 API 서비스 객체 생성
+from googleapiclient.discovery import build
 
-# Create your tests here.
+
+creds_filename = 'credentials.json'
+
+SCOPES = ['https://www.googleapis.com/auth/calendar']
+
+flow = InstalledAppFlow.from_client_secrets_file(creds_filename, SCOPES)
+creds = flow.run_local_server(port=0)
+
+today = datetime.date.today().isoformat()
+service = build('calendar', 'v3', credentials=creds)
+
+event = {
+    'summary': 'itsplay의 OpenAPI 수업',  # 일정 제목
+    'location': '서울특별시 성북구 정릉동 정릉로 77',  # 일정 장소
+    'description': 'itsplay와 OpenAPI 수업에 대한 설명입니다.',  # 일정 설명
+    'start': {  # 시작 날짜
+        'dateTime': today + 'T09:00:00',
+        'timeZone': 'Asia/Seoul',
+    },
+    'end': {  # 종료 날짜
+        'dateTime': today + 'T10:00:00',
+        'timeZone': 'Asia/Seoul',
+    },
+    'recurrence': [  # 반복 지정
+        'RRULE:FREQ=DAILY;COUNT=2'  # 일단위; 총 2번 반복
+    ],
+    'attendees': [  # 참석자
+        {'email': 'lpage@example.com'},
+        {'email': 'sbrin@example.com'},
+    ],
+    'reminders': {  # 알림 설정
+        'useDefault': False,
+        'overrides': [
+            {'method': 'email', 'minutes': 24 * 60},  # 24 * 60분 = 하루 전 알림
+            {'method': 'popup', 'minutes': 10},  # 10분 전 알림
+        ],
+    },
+}
+
+# calendarId : 캘린더 ID. primary이 기본 값입니다.
+event = service.events().insert(calendarId='primary', body=event).execute()
+print('Event created: %s' % (event.get('htmlLink')))
