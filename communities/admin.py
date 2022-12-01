9f68024b3a307379cf12d4d52255d0c746bbcd4c
avocad06 <miggo2704@gmail.com>
2022-12-01 23:07:39 +0900

    Style: 리뷰 디테일 스타일 추가 댓글로 바로 이동 기능 추가 댓글 영역 스타일 추가

--- communities/admin.py
+++ communities/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
-from .models import Article
+from .models import Article, ArticleComment
 
 # Register your models here.
 class PostAdmin(SummernoteModelAdmin):
     summernote_fields = ('content',)
 
-admin.site.register(Article, PostAdmin)
+admin.site.register(Article, PostAdmin,)
+admin.site.register(ArticleComment)
