3a5c375da44fd2d8c3071e6ef0bcf293b09e0147
JC <chohy1106@gmail.com>
2022-12-06 15:07:48 +0900

    Feat: 댓글 비동기 생성, 수정
    - 댓글 비동기 생성, 수정

--- communities/urls.py
+++ communities/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path("<str:country_code>/feed/", views.review, name="feed"),
     path("<str:country_code>/feed/feed_create", views.feed_create, name="feed_create"),
     path("<str:country_code>/review/<int:article_pk>/review_detail/comment_create/", views.article_comment_create, name="comment_create"),
-    path("<str:country_code>/review/<int:article_pk>/review_detail/comment_update/", views.article_comment_update, name="comment_create"),
+    path("<str:country_code>/review/<int:article_pk>/review_detail/<int:comment_pk>/comment_update/", views.article_comment_update, name="comment_create"),
     path("<str:country_code>/review/<int:article_pk>/review_detail/comment_delete/", views.article_comment_delete, name="comment_create"),
     path("test", views.test, name="test"),
 ]
