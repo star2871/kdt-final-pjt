41082b1b7f7b1e55db721bb1e9f4f85dc1197538
runedemonic <runedemonic@naver.com>
2022-12-15 00:13:55 +0900

    Chore: 정리

--- communities/admin.py
+++ communities/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
-from .models import Article, ArticleComment, Notice
+from .models import Article, ArticleComment, Notice, Feed
 
 # Register your models here.
 class PostAdmin(SummernoteModelAdmin):
@@ -9,3 +9,4 @@ class PostAdmin(SummernoteModelAdmin):
 admin.site.register(Article, PostAdmin,)
 admin.site.register(ArticleComment)
 admin.site.register(Notice)
+admin.site.register(Feed)
